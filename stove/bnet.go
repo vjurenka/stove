e4d3209e365436bc1703fdf5e10a175689d27274
laiqu <laiquendir@gmail.com>
2015-07-14 17:43:18 +0200

    WIP AS FUCK

--- stove/bnet.go
+++ stove/bnet.go
@@ -6,6 +6,7 @@ import (
 	"github.com/HearthSim/hs-proto/go"
 	"github.com/golang/protobuf/proto"
 	"hash/fnv"
+	"io"
 	"net"
 	"time"
 )
@@ -23,6 +24,7 @@ func check(e error) {
 
 type Service struct {
 	Name string
+	Id   uint32
 }
 
 func (s Service) GetHashedName() uint32 {
@@ -39,13 +41,47 @@ type session struct {
 	conn net.Conn
 }
 
+func HashToName(hash uint32) string {
+	hashmap := map[uint32]string{
+		3338259653: "bnet.protocol.game_master.GameFactorySubscriber",
+		3213656212: "bnet.protocol.channel.ChannelSubscriber",
+		2749215165: "bnet.protocol.friends.FriendsService",
+		1864735251: "bnet.protocol.friends.FriendsNotify",
+		3686756121: "bnet.protocol.challenge.ChallengeService",
+		101490829:  "bnet.protocol.channel.ChannelOwner",
+		1898188341: "bnet.protocol.authentication.AuthenticationClient",
+		1698982289: "bnet.protocol.connection.ConnectionService",
+		3073563442: "bnet.protocol.channel.Channel",
+		233634817:  "bnet.protocol.authentication.AuthenticationServer",
+		1423956503: "bnet.protocol.account.AccountNotify",
+		1467132723: "bnet.protocol.game_master.GameMasterSubscriber",
+		2165092757: "bnet.protocol.game_master.GameMaster",
+		3151632159: "bnet.protocol.challenge.ChallengeNotify",
+		1069623117: "bnet.protocol.game_utilities.GameUtilities",
+		2198078984: "bnet.protocol.channel_invitation.ChannelInvitationService",
+		4194801407: "bnet.protocol.presence.PresenceService",
+		3788189352: "bnet.protocol.notification.NotificationListener",
+		1658456209: "bnet.protocol.account.AccountService",
+		3971904954: "bnet.protocol.resources.Resources",
+		213793859:  "bnet.protocol.notification.NotificationService",
+	}
+	if hashmap[hash] != "" {
+		return hashmap[hash]
+	}
+	return "Unknown hash"
+}
+
 func (session *session) handleRequest(packet []byte) int {
 	headerSize := binary.BigEndian.Uint16(packet)
 	headerData := packet[2 : 2+int(headerSize)]
+	fmt.Printf("%x\n", headerData)
 
 	header := &hsproto.BnetProtocol_Header{}
 	err := proto.Unmarshal(headerData, header)
 	check(err)
+	if header.GetStatus() != 0 {
+		fmt.Println("header status != 0! :(")
+	}
 
 	packetEnd := 2 + int(headerSize) + int(header.GetSize())
 	bodyData := packet[2+headerSize : packetEnd]
@@ -56,9 +92,9 @@ func (session *session) handleRequest(packet []byte) int {
 		check(err)
 
 		// register services
-		connService := ConnectionService{Service{"bnet.protocol.connection.ConnectionService"}}
-		authServerService := AuthServerService{Service{"bnet.protocol.authentication.AuthenticationServer"}}
-		authClientService := AuthClientService{Service{"bnet.protocol.authentication.AuthenticationClient"}}
+		connService := ConnectionService{Service{"bnet.protocol.connection.ConnectionService", 0}}
+		authServerService := AuthServerService{Service{"bnet.protocol.authentication.AuthenticationServer", 1}}
+		authClientService := AuthClientService{Service{"bnet.protocol.authentication.AuthenticationClient", 255}}
 		fmt.Printf("connService=%d, authServerService=%d, authClientService=%d\n",
 			connService.GetHashedName(),
 			authServerService.GetHashedName(),
@@ -68,11 +104,11 @@ func (session *session) handleRequest(packet []byte) int {
 		bindRequest := body.GetBindRequest()
 		// iterate
 		for _, importedHash := range bindRequest.GetImportedServiceHash() {
-			fmt.Printf("Client imports service %x\n", importedHash)
+			fmt.Printf("Client imports service %d probably: %s\n", importedHash, HashToName(importedHash))
 		}
 
 		for _, export := range bindRequest.GetExportedService() {
-			fmt.Printf("Client exports service id=%d, hash=%x\n", export.GetId(), export.GetHash())
+			fmt.Printf("Client exports service id=%d, hash=%d probably: %s\n", export.GetId(), export.GetHash(), HashToName(export.GetHash()))
 		}
 
 		timestamp := uint64(time.Now().UnixNano() / 1000)
@@ -87,26 +123,245 @@ func (session *session) handleRequest(packet []byte) int {
 				Label: proto.Uint32(1255760),
 				Epoch: proto.Uint32(epoch),
 			},
-			BindResponse: &hsproto.BnetProtocolConnection_BindResponse{
-				ImportedServiceId: []uint32{1, 7, 8, 9, 6, 10, 11, 3, 2, 12, 5, 4}, //  5, 8, 1, 9, 3, 10, 11, 2, 12, 7, 13, 6, 14, 4, 15},
-			},
 			BindResult: proto.Uint32(0),
+			BindResponse: &hsproto.BnetProtocolConnection_BindResponse{
+				ImportedServiceId: []uint32{
+					1,
+					2,
+					3,
+					4,
+					5,
+					6,
+					7,
+					8,
+					9,
+					10,
+					11,
+					12,
+				},
+			},
 			ServerTime: proto.Uint64(timestamp),
 		}
 
 		data, err := proto.Marshal(resp)
 		check(err)
-
 		header := &hsproto.BnetProtocol_Header{
 			ServiceId: proto.Uint32(254),
 			MethodId:  proto.Uint32(1),
-			Token:     proto.Uint32(0),
+			Token:     proto.Uint32(header.GetToken()),
 			Size:      proto.Uint32(uint32(len(data))),
 		}
 
 		session.writePacket(header, data)
+	} else if header.GetServiceId() == 1 && header.GetMethodId() == 1 {
+		fmt.Println("Auth Logon")
+		body := &hsproto.BnetProtocolAuthentication_LogonRequest{}
+		err = proto.Unmarshal(bodyData, body)
+		check(err)
+
+		header := &hsproto.BnetProtocol_Header{
+			ServiceId: proto.Uint32(254),
+			Token:     proto.Uint32(header.GetToken()),
+			Size:      proto.Uint32(0),
+			Status:    proto.Uint32(0),
+		}
+		session.writePacket(header, make([]byte, 0))
+
+		resp := &hsproto.BnetProtocolAuthentication_LogonQueueUpdateRequest{
+			Position:          proto.Uint32(0),
+			EstimatedTime:     proto.Uint64(0),
+			EtaDeviationInSec: proto.Uint64(0),
+		}
+		data, err := proto.Marshal(resp)
+		check(err)
+		header = &hsproto.BnetProtocol_Header{
+			ServiceId: proto.Uint32(1),
+			MethodId:  proto.Uint32(12),
+			Token:     proto.Uint32(0),
+			Size:      proto.Uint32(uint32(len(data))),
+		}
+		session.writePacket(header, data)
+
+		respUpd := &hsproto.BnetProtocolAuthentication_LogonUpdateRequest{
+			ErrorCode: proto.Uint32(0),
+		}
+		data, err = proto.Marshal(respUpd)
+		check(err)
+		header = &hsproto.BnetProtocol_Header{
+			ServiceId: proto.Uint32(1),
+			MethodId:  proto.Uint32(10),
+			Token:     proto.Uint32(2),
+			Size:      proto.Uint32(uint32(len(data))),
+		}
+		session.writePacket(header, data)
+
+		respResult := &hsproto.BnetProtocolAuthentication_LogonResult{
+			ErrorCode: proto.Uint32(0),
+			Account: &hsproto.BnetProtocol_EntityId{
+				High: proto.Uint64(1),
+				Low:  proto.Uint64(0),
+			},
+			GameAccount: []*hsproto.BnetProtocol_EntityId{
+				&hsproto.BnetProtocol_EntityId{
+					High: proto.Uint64(2),
+					Low:  proto.Uint64(0),
+				},
+			},
+			ConnectedRegion: proto.Uint32(0),
+		}
+		data, err = proto.Marshal(respResult)
+		check(err)
+		header = &hsproto.BnetProtocol_Header{
+			ServiceId: proto.Uint32(1),
+			MethodId:  proto.Uint32(5),
+			Token:     proto.Uint32(3),
+			Size:      proto.Uint32(uint32(len(data))),
+		}
+		session.writePacket(header, data)
+	} else if header.GetServiceId() == 1 && header.GetMethodId() == 4 {
+		fmt.Println("Auth SelectGameAccount")
+		header = &hsproto.BnetProtocol_Header{
+			ServiceId: proto.Uint32(254),
+			Token:     proto.Uint32(header.GetToken()),
+			Size:      proto.Uint32(0),
+			Status:    proto.Uint32(0),
+		}
+		session.writePacket(header, make([]byte, 0))
+	} else if header.GetServiceId() == 5 && header.GetMethodId() == 1 {
+		fmt.Println("Presence Subscribe")
+		header = &hsproto.BnetProtocol_Header{
+			ServiceId: proto.Uint32(254),
+			Token:     proto.Uint32(header.GetToken()),
+			Size:      proto.Uint32(0),
+			Status:    proto.Uint32(0),
+		}
+		session.writePacket(header, make([]byte, 0))
+	} else if header.GetServiceId() == 11 && header.GetMethodId() == 30 {
+		fmt.Println("Account GetAccountState")
+		count := "EU"
+		resp := &hsproto.BnetProtocolAccount_GetAccountStateResponse{
+			State: &hsproto.BnetProtocolAccount_AccountState{
+				AccountLevelInfo: &hsproto.BnetProtocolAccount_AccountLevelInfo{
+					Licenses: []*hsproto.BnetProtocolAccount_AccountLicense{
+						&hsproto.BnetProtocolAccount_AccountLicense{
+							Id: proto.Uint32(0),
+						},
+					},
+					DefaultCurrency: proto.Uint32(0),
+					Country:         &count,
+					PreferredRegion: proto.Uint32(0),
+				},
+			},
+		}
+		data, err := proto.Marshal(resp)
+		check(err)
+		header = &hsproto.BnetProtocol_Header{
+			ServiceId: proto.Uint32(254),
+			Token:     proto.Uint32(header.GetToken()),
+			Size:      proto.Uint32(uint32(len(data))),
+			Status:    proto.Uint32(0),
+		}
+		session.writePacket(header, data)
+	} else if header.GetServiceId() == 9 && header.GetMethodId() == 1 {
+		fmt.Println("Friends SubscribeToFriends")
+		resp := &hsproto.BnetProtocolFriends_SubscribeToFriendsResponse{
+			MaxFriends:             proto.Uint32(100),
+			MaxReceivedInvitations: proto.Uint32(42),
+			MaxSentInvitations:     proto.Uint32(7),
+		}
+		data, err := proto.Marshal(resp)
+		check(err)
+		header = &hsproto.BnetProtocol_Header{
+			ServiceId: proto.Uint32(254),
+			Token:     proto.Uint32(header.GetToken()),
+			Size:      proto.Uint32(uint32(len(data))),
+			Status:    proto.Uint32(0),
+		}
+		session.writePacket(header, data)
+	} else if header.GetServiceId() == 12 && header.GetMethodId() == 1 {
+		fmt.Println("Resources GetContentHandle")
+		contentRequest := &hsproto.BnetProtocolResources_ContentHandleRequest{}
+		err := proto.Unmarshal(bodyData, contentRequest)
+		fmt.Printf("%d %d\n", contentRequest.GetProgramId(), contentRequest.GetStreamId())
+		resp := &hsproto.BnetProtocol_ContentHandle{
+			Region: proto.Uint32(0),
+			Usage:  proto.Uint32(0),
+			Hash:   make([]byte, 0),
+		}
+		data, err := proto.Marshal(resp)
+		check(err)
+		header = &hsproto.BnetProtocol_Header{
+			ServiceId: proto.Uint32(254),
+			Token:     proto.Uint32(header.GetToken()),
+			Size:      proto.Uint32(uint32(len(data))),
+			Status:    proto.Uint32(0),
+		}
+		session.writePacket(header, data)
+	} else if header.GetServiceId() == 8 && header.GetMethodId() == 1 {
+		fmt.Println("ChannelInvitation Subscribe")
+		resp := &hsproto.BnetProtocolChannelInvitation_SubscribeResponse{}
+		data, err := proto.Marshal(resp)
+		check(err)
+		header = &hsproto.BnetProtocol_Header{
+			ServiceId: proto.Uint32(254),
+			Token:     proto.Uint32(header.GetToken()),
+			Size:      proto.Uint32(uint32(len(data))),
+			Status:    proto.Uint32(0),
+		}
+		session.writePacket(header, data)
+	} else if header.GetServiceId() == 2 && header.GetMethodId() == 1 {
+		fmt.Println("GameUtilities ClientRequest")
+		clientRequest := &hsproto.BnetProtocolGameUtilities_ClientRequest{}
+		proto.Unmarshal(bodyData, clientRequest)
+		if len(clientRequest.GetAttribute()) != 2 {
+			fmt.Println("Too many attributes in pegasus client request?")
+		}
+		packetType := int64(-1)
+		for _, att := range clientRequest.GetAttribute() {
+			fmt.Println(att.GetName())
+			if att.GetName() == "p" {
+				fmt.Printf("%x\n", att.GetValue().GetBlobValue())
+				blob := att.GetValue().GetBlobValue()
+				if len(blob) < 2 {
+					fmt.Println("blob is too short")
+				} else {
+					packetType = int64(blob[0]) + int64(blob[1])<<8
+				}
+			}
+		}
+		fmt.Printf("packet type %d\n", packetType)
+		name := "it shouldn't matter"
+		responseType := int64(packetType + 1) // looks like response type is request + 1
+		resp := &hsproto.BnetProtocolGameUtilities_ClientResponse{
+			Attribute: []*hsproto.BnetProtocolAttribute_Attribute{
+				&hsproto.BnetProtocolAttribute_Attribute{
+					Name: &name,
+					Value: &hsproto.BnetProtocolAttribute_Variant{
+						//IntValue: &packetType,
+						IntValue: &responseType,
+					},
+				},
+				&hsproto.BnetProtocolAttribute_Attribute{
+					Name: &name,
+					Value: &hsproto.BnetProtocolAttribute_Variant{
+						BlobValue: []byte{},
+					},
+				},
+			},
+		}
+		data, err := proto.Marshal(resp)
+		check(err)
+		header = &hsproto.BnetProtocol_Header{
+			ServiceId: proto.Uint32(254),
+			Token:     proto.Uint32(header.GetToken()),
+			Size:      proto.Uint32(uint32(len(data))),
+			Status:    proto.Uint32(0),
+		}
+		session.writePacket(header, data)
+	} else if header.GetServiceId() == 0 && header.GetMethodId() == 5 {
+		fmt.Println("Keep alive!")
 	} else {
-		fmt.Printf("unsupported: %d, %d\n", header.GetServiceId(), header.GetMethodId())
+		fmt.Printf("unsupported: %d, %d, bodylen: %d\n %x\nheader: %x\n", header.GetServiceId(), header.GetMethodId(), len(bodyData), string(bodyData[:]), string(headerData[:]))
 	}
 	return packetEnd
 }
@@ -134,10 +389,14 @@ func (session *session) writePacket(head *hsproto.BnetProtocol_Header, body []by
 func (session *session) serve() {
 	defer session.conn.Close()
 
-	buf := make([]byte, 1024)
+	buf := make([]byte, 4096)
 	idx := 0
 	for {
 		read, err := session.conn.Read(buf[idx:])
+		if err == io.EOF {
+			fmt.Println("EOF?")
+			break
+		}
 		check(err)
 		totalProcessed := 0
 		for read > totalProcessed {
