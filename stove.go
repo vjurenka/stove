44a755d8b63abda2ed250414f5b6a1e6d70efbd4
Jerome Leclanche <jerome@leclan.ch>
2015-07-21 03:08:33 +0200

    Add some output on startup

--- stove.go
+++ stove.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"github.com/HearthSim/stove/bnet"
 	"github.com/HearthSim/stove/pegasus"
+)
+
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = 1119
 )
 
 func main() {
 	serv := bnet.NewServer()
 	serv.RegisterGameServer("WTCG", pegasus.NewServer(serv))
-	serv.ListenAndServe("localhost:1119")
+
+	addr := fmt.Sprintf("%s:%d", CONN_HOST, CONN_PORT)
+	fmt.Printf("Listening on %s ...\n", addr)
+	serv.ListenAndServe(addr)
 }
