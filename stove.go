605814927902850443d17e6f5fb88c06e9246809
Simon Pinfold <simon@uint8.me>
2015-08-08 02:51:07 +1200

    Optionally get the bind address from the command line

--- stove.go
+++ stove.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"flag"
+	"strings"
 	"fmt"
 	"github.com/HearthSim/stove/bnet"
 	"github.com/HearthSim/stove/pegasus"
 )
 
 const (
-	CONN_HOST = "localhost"
-	CONN_PORT = 1119
+	CONN_DEFAULT_HOST = "localhost"
+	CONN_DEFAULT_PORT = 1119
 )
 
 func main() {
+	addr := fmt.Sprintf("%s:%d", CONN_DEFAULT_HOST, CONN_DEFAULT_PORT)
+	flag.StringVar(&addr, "bind", addr, "The address to run on")
+	flag.Parse()
+
+	if !strings.Contains(addr, ":") {
+		addr = fmt.Sprintf("%s:%d", addr, CONN_DEFAULT_PORT)
+	}
+
 	serv := bnet.NewServer()
 	serv.RegisterGameServer("WTCG", pegasus.NewServer(serv))
 
-	addr := fmt.Sprintf("%s:%d", CONN_HOST, CONN_PORT)
 	fmt.Printf("Listening on %s ...\n", addr)
 	serv.ListenAndServe(addr)
 }
