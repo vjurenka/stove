4016e94fa215e0f0b385329a4e21cf2655c425e0
Simon Pinfold <simon@uint8.me>
2015-08-15 16:50:24 +1200

    Implement OnDraftBegin

--- pegasus/draft.go
+++ pegasus/draft.go
@@ -3,10 +3,62 @@ package pegasus
 import (
 	"github.com/HearthSim/hs-proto/go"
 	"github.com/golang/protobuf/proto"
+	"time"
 )
 
 func (s *Draft) Init(sess *Session) {
+	sess.RegisterUtilHandler(0, 235, OnDraftBegin)
 	sess.RegisterUtilHandler(0, 244, OnDraftGetPicksAndContents)
+}
+
+func MakeHeroChoices() (choices []DraftChoice) {
+	favoriteHeroes := []FavoriteHero{}
+	db.Limit(3).Find(&favoriteHeroes)
+	for i := 1; i <= 3; i++ {
+		choices = append(choices, DraftChoice{
+			CardID:      favoriteHeroes[i-1].CardID,
+			ChoiceIndex: i,
+			Slot:        0,
+		})
+	}
+	return choices
+}
+
+func ChoicesToCardDefs(choices []DraftChoice) (defs []*hsproto.PegasusShared_CardDef) {
+	for _, choice := range choices {
+		defs = append(defs, &hsproto.PegasusShared_CardDef{
+			Asset:   proto.Int32(choice.CardID),
+			Premium: proto.Int32(int32(0)),
+		})
+	}
+	return defs
+}
+
+func OnDraftBegin(s *Session, body []byte) ([]byte, error) {
+	deck := Deck{
+		AccountID:    s.Account.ID,
+		DeckType:     int(hsproto.PegasusShared_DeckType_DRAFT_DECK),
+		Name:         "Arena Deck",
+		CardBackID:   0, //TODO
+		LastModified: time.Now().UTC(),
+	}
+	db.Create(&deck)
+
+	draft := Draft{
+		AccountID:   s.Account.ID,
+		DeckID:      deck.ID,
+		CurrentSlot: 0,
+		Choices:     MakeHeroChoices(),
+		Ended:       false,
+	}
+	db.Create(&draft)
+
+	choiceList := ChoicesToCardDefs(draft.Choices)
+	res := hsproto.PegasusUtil_DraftBeginning{
+		DeckId:     proto.Int64(deck.ID),
+		ChoiceList: choiceList,
+	}
+	return EncodeUtilResponse(246, &res)
 }
 
 func OnDraftGetPicksAndContents(s *Session, body []byte) ([]byte, error) {
